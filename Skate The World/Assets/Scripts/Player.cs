4a14fc3365870f25baf82a2448860d1845c4025f
fzlink <fazlibozatay@gmail.com>
2019-10-14 20:48:13 +0300

    Added Curved World

---
+++ Skate The World/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0,0,20f);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        FindObjectOfType<Game>().spawnRoad();
+    }
+}
